948477da7a81d514eeaf55c9c541d9e77e1dc437
Gaëtan <blaxoujunior@gmail.com>
2024-03-13 15:13:15 +0100

    feat: support animations

--- example/test/webp_test.dart
+++ example/test/webp_test.dart
@@ -1,16 +1,17 @@
-import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image/image.dart' as image;
 import 'package:libwebp/libwebp.dart';
 import 'package:path/path.dart' as path;
 
-void main() {
-  final xdd = base64Decode(
-    'UklGRpQDAABXRUJQVlA4IIgDAABQEACdASo5ACAAPhkIg0EhBn7NAAQAYSygE6ZXsD33bj1YOesxoPEU6Y3PSaabvOVdSfZvAfwS+Z5DNwH1Cwy/3jwB9zeHreCOP/0HiI0o80Hyf/T4YKCDiHhQIqZB0RN0iU7JCTMtTXna2eEfpXJykcq3udU04hwboTkU2juV5s0lUIqlE0IWzKwAAP7/l3fwbHVdt5lcKRUdL5CfGTe07/JoQ8Jdf6AknDnzh78GbWS/nvab9lpb9JEFs4DPYp9hRNEKivgTdOb2fpuMb2ayZ83+LWrv7l+anqA9f/5VkaMkNn34nVh36nKVTqv2Jl0zdpNwpxqc3TmyZdigJFTjQUX4VIiNJNusTm9W4tsW/TZ1fEEvhxoQTiLi/dF2WfUVbC0UkpnBnhUw54iGwRrIHf2Qagna0HG17C9UKSvw/z1eoofJ8QypE+fmW2x3H/SD2fdw2K1rcf/CYpraaX2tTHPdc33luwSz/XqV+r4j/UXCUA6M2U30D2UVT+UBibCB7yCHYkpzcH8RO/An7vr+AYam60PVcKd4nhgtaFaW7HR4Sqzs9uQs8IZc9Y3HCkY1hUvih3GRHw82v4imrACqwqchpST77jOGgZAB49E2eaqgDrfFId0GZrkkxhv4tkAyPHNDa+ZyD6CSgLiEH8VaI8oE9WDI8eWbB7bNsUh+mJ7tfdEdhlr6ZD8u2kT3+wsiFmxl9+AVy/O7VXUNhihdbanO9if+pTMJGfxsgZV3hkue9WzL31twDLUAJ19Xl94QNHcsCSOUYOWc9XD60TineBDObLGHOc1mgiks7SCOx20EYwPvqfQdJS6I2604dGvcES9Sdptel37CEkPfm/lw0Ty7RHa561k4CvbN+Xt5WH/HWdbEx5s5L/JcCVOMyxFkSeLArRgmy+2B56+FLFu5zcUsbQujZtMp89Sq627EUtYfBruPt5PU5HDn8/xOrhGyIe9TWyww2rSatR5NJxch+f56q44+ohNctzXMhkC9yhqHG3/ZuE09VooJeHHrrMepyKLuTYGZSJhVLlj0Ysb9PQp51J0D5RiQ2C/GglQslXySH4hMbS+pDUcdPyCvl+BvImBwyOsTAooTNGooomtB4XEOaaOzno9r0A6LQ5Y75Qc8iHyJs9B/BydrUFCnHlQkv4Qgp4+MqvLtaeZZXT/Ljfci36Q9XHt6RaM40ELB/Sge9mrtFAAA',
-  );
+Future<Uint8List> load(String name) async {
+  final file = File(path.join('test', 'assets', name));
+  return file.readAsBytes();
+}
 
+void main() {
   late Directory temp;
 
   setUpAll(() async {
@@ -20,19 +21,36 @@ void main() {
     print('temp: ${temp.path}');
   });
 
-  test('xdd right dimensions', () {
-    final img = image.decodeWebP(xdd)!;
-    expect(img.width, 57);
-    expect(img.height, 32);
+  test('xdd right dimensions', () async {
+    final img = image.decodeWebP(await load('xdd.webp'))!;
+    expect(img.width, 228);
+    expect(img.height, 128);
   });
 
-  test('rescale', () async {
+  test('rescale xdd', () async {
     print('platform: ${Platform.operatingSystem}');
-    await temp.file('xdd.webp').writeAsBytes(xdd);
+    final xdd = await load('xdd.webp');
 
     final img = resizeWebp(xdd, (width: 512, height: 512));
 
     await temp.file('xdd-512x512.webp').writeAsBytes(img);
+
+    final resized = image.decodeWebP(img);
+    if (resized == null) {
+      throw Exception('Failed to decode resized image.');
+    }
+    expect(resized.width, 512);
+    expect(resized.height, 512);
+  });
+
+  test('rescale xdding (animated)', () async {
+    print('platform: ${Platform.operatingSystem}');
+    final xdd = await load('xdding.webp');
+
+    final img = resizeWebp(xdd, (width: 512, height: 512));
+
+    await temp.file('xdding-512x512.webp').writeAsBytes(img);
+    print('xdding-512x512.webp: ${temp.file('xdding-512x512.webp').path}');
 
     final resized = image.decodeWebP(img);
     if (resized == null) {
--- lib/libwebp.dart
+++ lib/libwebp.dart
@@ -6,6 +6,8 @@ import 'package:ffi/ffi.dart';
 import 'package:libwebp/libwebp_generated_bindings.dart';
 import 'package:libwebp/src/utils.dart';
 
+bool get isTest => Platform.environment['FLUTTER_TEST'] == 'true';
+
 const String _libName = 'libwebp_flutter_libs';
 
 /// The dynamic library in which the symbols for [LibwebpFlutterLibsBindings] can be found.
@@ -14,6 +16,11 @@ final DynamicLibrary _dylib = () {
     return DynamicLibrary.open('$_libName.framework/$_libName');
   }
   if (Platform.isMacOS) {
+    if (isTest) {
+      return DynamicLibrary.open(
+          'build/macos/Build/Products/Release/libwebp/libwebp.framework/libwebp');
+    }
+
     const libName = 'libwebp';
 
     return DynamicLibrary.open('$libName.framework/$libName');
@@ -43,53 +50,148 @@ final libwebp = LibwebpFlutterLibsBindings(_dylib);
 }
 
 Uint8List resizeWebp(
-        Uint8List input, ({int? width, int? height}) targetDimensions) =>
+  Uint8List input,
+  ({int width, int height}) targetDimensions,
+) =>
     using((Arena alloc) {
       final data = alloc.uint8Array(input.length);
       data.asList.setAll(0, input);
       final curr = getWebpDimensions(data);
 
-      final decoderConfig = alloc<WebPDecoderConfig>();
-      libwebp.WebPInitDecoderConfigInternal(
-        decoderConfig,
-        WEBP_DECODER_ABI_VERSION,
-      );
-
-      decoderConfig.ref.output.colorspace = WEBP_CSP_MODE.MODE_RGBA;
-      decoderConfig.ref.output.width = curr.width;
-      decoderConfig.ref.output.height = curr.height;
-
-      decoderConfig.ref.options.use_scaling = 1;
-      decoderConfig.ref.options.scaled_width = targetDimensions.width ?? 0;
-      decoderConfig.ref.options.scaled_height = targetDimensions.height ?? 0;
-
-      final res2 = libwebp.WebPDecode(data.ptr, data.length, decoderConfig);
-      if (res2 != VP8StatusCode.VP8_STATUS_OK) {
-        throw LibWebpException('Failed to decode WebP, got status code $res2.');
-      }
-
-      final outPtr = alloc<Pointer<Uint8>>();
-
-      final outputSize = libwebp.WebPEncodeRGBA(
-        decoderConfig.ref.output.u.RGBA.rgba,
-        decoderConfig.ref.output.width,
-        decoderConfig.ref.output.height,
-        decoderConfig.ref.output.u.RGBA.stride,
-        100,
-        outPtr,
-      );
-
-      if (outputSize == 0) {
-        throw LibWebpException('Failed to encode WebP.');
-      }
-
-      final outList = outPtr.value.asTypedList(outputSize);
-      final outData = Uint8List.fromList(outList);
-
-      libwebp.WebPFree(outPtr.value as Pointer<Void>);
+      Uint8List outData = _resizeWebp(alloc, curr, targetDimensions, data);
 
       return outData;
     });
+
+Uint8List _resizeWebp(
+  Arena alloc,
+  ({int height, int width}) curr,
+  ({int height, int width}) targetDimensions,
+  Uint8Data data,
+) {
+  final encoder = libwebp.WebPAnimEncoderNewInternal(
+    targetDimensions.width,
+    targetDimensions.height,
+    nullptr,
+    WEBP_DEMUX_ABI_VERSION,
+  );
+
+  final cfg = alloc<WebPConfig>();
+  _check(
+    libwebp.WebPConfigInitInternal(
+      cfg,
+      WebPPreset.WEBP_PRESET_DEFAULT,
+      75,
+      WEBP_ENCODER_ABI_VERSION,
+    ),
+    'Failed to init WebPConfig.',
+  );
+  cfg.ref.thread_level = 1;
+
+  final animDecoder = _animDecoder(alloc, data);
+  final info = alloc<WebPAnimInfo>();
+  _check(
+      libwebp.WebPAnimDecoderGetInfo(animDecoder, info), 'Failed to get info.');
+  print('''
+  info:
+    canvas_width: ${info.ref.canvas_width}
+    canvas_height: ${info.ref.canvas_height}
+    loop_count: ${info.ref.loop_count}
+    bgcolor: ${info.ref.bgcolor}
+    frame_count: ${info.ref.frame_count}
+  ''');
+
+  final timestamp = alloc<Int>();
+  final buf = alloc<Pointer<Uint8>>();
+  for (int i = 0; i < info.ref.frame_count; i++) {
+    // print('frame $i');
+    _check(
+      libwebp.WebPAnimDecoderGetNext(animDecoder, buf, timestamp),
+      'Failed to get next frame.',
+    );
+
+    final frame = alloc<WebPPicture>();
+    _check(
+      libwebp.WebPPictureInitInternal(frame, WEBP_ENCODER_ABI_VERSION),
+      'Failed to init WebPPicture.',
+    );
+    frame.ref.use_argb = 1; // use ARGB
+    frame.ref.width = curr.width;
+    frame.ref.height = curr.height;
+    _check(
+      libwebp.WebPPictureAlloc(frame),
+      'Failed to allocate WebPPicture.',
+    );
+    _check(
+      libwebp.WebPPictureImportRGBA(frame, buf.value, curr.width * 4),
+      'Failed to import frame $i to WebPPicture.',
+    );
+    _check(
+      libwebp.WebPPictureRescale(
+        frame,
+        targetDimensions.width,
+        targetDimensions.height,
+      ),
+      'Failed to rescale frame $i.',
+    );
+
+    final added = libwebp.WebPAnimEncoderAdd(
+      encoder,
+      frame,
+      timestamp.value,
+      cfg,
+    );
+    if (added == 0) {
+      final str = libwebp.WebPAnimEncoderGetError(encoder) as Pointer<Utf8>;
+      throw LibWebpException(
+          'Failed to add frame $i to encoder. (${VP8StatusCode.fromValue(frame.ref.error_code)}, ${str.toDartString()})');
+    }
+
+    libwebp.WebPPictureFree(frame);
+  }
+  final added =
+      libwebp.WebPAnimEncoderAdd(encoder, nullptr, timestamp.value, nullptr);
+  if (added == 0) {
+    throw LibWebpException('Failed to add frame null frame to encoder.');
+  }
+
+  final out = alloc<WebPData>();
+  final size = libwebp.WebPAnimEncoderAssemble(encoder, out);
+  if (size == 0) {
+    throw LibWebpException('Failed to assemble WebP.');
+  }
+
+  final uint8list = Uint8List.fromList(out.ref.bytes.asTypedList(out.ref.size));
+
+  libwebp.WebPAnimDecoderDelete(animDecoder);
+  libwebp.WebPAnimEncoderDelete(encoder);
+
+  return uint8list;
+}
+
+int _hasMoreFrames(Pointer<WebPAnimDecoder> animDecoder) =>
+    libwebp.WebPAnimDecoderHasMoreFrames(animDecoder);
+
+Pointer<WebPAnimDecoder> _animDecoder(Allocator a, Uint8Data data) {
+  final webpData = a<WebPData>();
+  webpData.ref.bytes = data.ptr;
+  webpData.ref.size = data.length;
+
+  final opt = a<WebPAnimDecoderOptions>();
+  libwebp.WebPAnimDecoderOptionsInitInternal(opt, WEBP_DEMUX_ABI_VERSION);
+  opt.ref.color_mode = WEBP_CSP_MODE.MODE_RGBA;
+
+  final decoder = libwebp.WebPAnimDecoderNewInternal(
+    webpData,
+    opt,
+    WEBP_DEMUX_ABI_VERSION,
+  );
+  if (decoder == nullptr) {
+    throw LibWebpException('Failed to create WebPAnimDecoder.');
+  }
+
+  return decoder;
+}
 
 class LibWebpException implements Exception {
   final String message;
@@ -98,4 +200,29 @@ class LibWebpException implements Exception {
 
   @override
   String toString() => 'LibWebpException: $message';
+}
+
+enum VP8StatusCode {
+  ok(0),
+  outOfMemory(1),
+  invalidParam(2),
+  bitstreamError(3),
+  unsupportedFeature(4),
+  suspended(5),
+  userAbort(6),
+  notEnoughData(7),
+  ;
+
+  final int value;
+  const VP8StatusCode(this.value);
+
+  factory VP8StatusCode.fromValue(int value) {
+    return VP8StatusCode.values.firstWhere((e) => e.value == value);
+  }
+}
+
+void _check(int res, [String? message]) {
+  if (res == 0) {
+    throw LibWebpException(message ?? 'Failed with error code $res.');
+  }
 }
